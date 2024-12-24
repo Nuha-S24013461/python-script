8d540853f39dc34fc7e3af002dd62708c25f0db9
Nuha-S24013461 <s24013461@ousl.lk>
2024-12-24 20:31:13 +0530

    hello.py

--- hello.py
+++ hello.py
@@ -1,2 +1,16 @@
 # python-script
 print("Hello GitHub")
+import requests
+username = "your-username"
+repo_name = "python-scripts"
+file_path = "hello.py"
+
+url = f"https://github.com/Nuha-S24013461/python-script/edit/main/hello.py"
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Fetched script content:")
+    print(response.text)
+else:
+    print(f"Failed to fetch script. Status code: {response.status_code}")
+
